4de88e7deba96e0647b6b176f22abfdfa9e5a76b
Josiah Sayers <josiah.sayers15@gmail.com>
2020-01-10 01:14:50 -0500

    adds finished app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,21 @@ import {
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 import { LoginComponent } from './auth/login/login.component';
 import { UserService } from './services/auth/user-service';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+
+// When running in production mode Angular is not picking up this value from the environment file :(
+const firebaseConfig = {
+  apiKey: 'AIzaSyBwmWk7HZ0nqSuf1AgKm4-EDiR-aAaeonQ',
+  authDomain: 'fir-dev-chapter.firebaseapp.com',
+  databaseURL: 'https://fir-dev-chapter.firebaseio.com',
+  projectId: 'fir-dev-chapter',
+  storageBucket: 'fir-dev-chapter.appspot.com',
+  messagingSenderId: '546625539681',
+  appId: '1:546625539681:web:2af3ba786a7b354b7f6420',
+  measurementId: 'G-S740GFS6LJ'
+};
 
 @NgModule({
   declarations: [
@@ -40,11 +55,16 @@ import { UserService } from './services/auth/user-service';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [
     MessageDatabaseServiceService,
-    UserService
+    UserService,
+    AngularFireAuth,
+    AngularFirestore
   ],
   bootstrap: [AppComponent]
 })
